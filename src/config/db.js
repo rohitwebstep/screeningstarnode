a4dbeb14d098f4385d1e420986bfd9ab471d341c
rohitwebstep <165655930+rohitwebstep@users.noreply.github.com>
2024-08-13 15:19:54 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
