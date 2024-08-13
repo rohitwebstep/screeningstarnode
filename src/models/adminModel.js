5271cfc86b7711b05ded1e1282f2e907b01a5083
rohitwebstep <rohitwebstep@gmail.com>
2024-08-13 15:49:34 +0530

    MySQL Error Solved

--- src/models/adminModel.js
+++ src/models/adminModel.js
@@ -1,21 +1,43 @@
-const db = require('../config/db');
+const pool = require('../config/db');
 
 const Admin = {
   findByEmailOrMobile: (username, callback) => {
+    // Log the query operation
+    console.log('Querying findByEmailOrMobile for:', username);
     const sql = `
       SELECT * FROM \`admins\`
       WHERE \`email\` = ? OR \`mobile\` = ?
     `;
-    db.query(sql, [username, username], callback);
+    pool.query(sql, [username, username], (err, results) => {
+      if (err) {
+        // Log database query errors
+        console.error('Database query error:', err);
+        return callback(err, null);
+      }
+      // Log the results of the query
+      console.log('Query results:', results);
+      callback(null, results);
+    });
   },
 
   validatePassword: (username, password, callback) => {
+    // Log the query operation
+    console.log('Querying validatePassword for:', username);
     const sql = `
       SELECT * FROM \`admins\`
       WHERE (\`email\` = ? OR \`mobile\` = ?)
       AND \`password\` = MD5(?)
     `;
-    db.query(sql, [username, username, password], callback);
+    pool.query(sql, [username, username, password], (err, results) => {
+      if (err) {
+        // Log database query errors
+        console.error('Database query error:', err);
+        return callback(err, null);
+      }
+      // Log the results of the query
+      console.log('Query results:', results);
+      callback(null, results);
+    });
   },
 };
 
