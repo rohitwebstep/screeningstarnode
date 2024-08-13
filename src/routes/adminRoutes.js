f4407c5362b8a21ba52c0c190746e06286e59bc0
rohitwebstep <rohitwebstep@gmail.com>
2024-08-13 18:53:39 +0530

    correction

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -1,12 +1,8 @@
  const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/admin/authController');
-const serviceController = require('../controllers/serviceController');
 
 // Authentication routes
 router.post('/login', authController.login);
-
-// Service routes
-router.get('/services', serviceController.getServices);
 
 module.exports = router;
