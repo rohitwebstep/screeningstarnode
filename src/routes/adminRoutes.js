efcfef6abb9af311951cea41c6738aa7752205c7
rohitwebstep <rohitwebstep@gmail.com>
2024-08-14 15:50:33 +0530

    for valildate login or not

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/admin/authController');
 
 // Authentication routes
 router.post('/login', authController.login);
+router.post('/verify-login', authController.ValidateLogin);
 
 module.exports = router;
