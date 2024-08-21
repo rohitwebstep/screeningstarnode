3e3c88d040dd008d7a3b1fcabf67d4407aabccee
rohitwebstep <rohitwebstep@gmail.com>
2024-08-21 13:55:15 +0530

    For Branche

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const adminRoutes = require('./routes/admin/indexRoutes');
-const customerRoutes = require('./routes/customer/indexRoutes');
-const packageRoutes = require('./routes/admin/packageRoutes');
-const serviceRoutes = require('./routes/admin/serviceRoutes');
-require('dotenv').config();  // Ensure you load environment variables
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const adminRoutes = require("./routes/admin/indexRoutes");
+const customerRoutes = require("./routes/customer/indexRoutes");
+const branchRoutes = require("./routes/customer/branch/branchRoutes");
+const packageRoutes = require("./routes/admin/packageRoutes");
+const serviceRoutes = require("./routes/admin/serviceRoutes");
+require("dotenv").config(); // Ensure you load environment variables
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,10 +14,11 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/admin', adminRoutes);
-app.use('/customer', customerRoutes);
-app.use('/package', packageRoutes);
-app.use('/service', serviceRoutes);
+app.use("/admin", adminRoutes);
+app.use("/customer", customerRoutes);
+app.use("/branch", branchRoutes);
+app.use("/package", packageRoutes);
+app.use("/service", serviceRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
--- src/routes/customer/indexRoutes.js
+++ src/routes/customer/indexRoutes.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../../controllers/customer/authController');
-const profileController = require('../../controllers/customer/profileController');
+const authController = require("../../controllers/customer/authController");
+const profileController = require("../../controllers/customer/profileController");
 
 // Authentication routes
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.post('/verify-admin-login', authController.validateLogin);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
+router.post("/verify-admin-login", authController.validateLogin);
 
 // Profile routes
-router.post('/create', profileController.create);
+router.post("/create", profileController.create);
 
 module.exports = router;
