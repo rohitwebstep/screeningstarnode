0bd51c9da09fdd3436ef649adcd25c0391b65750
rohitwebstep <rohitwebstep@gmail.com>
2024-08-21 16:28:18 +0530

    for customers

--- src/models/customer/customerModel.js
+++ src/models/customer/customerModel.js
@@ -1,8 +1,9 @@
 const crypto = require("crypto");
-const pool = require('../../config/db');
+const pool = require("../../config/db");
 
 // Function to hash the password using MD5
-const hashPassword = (password) => crypto.createHash('md5').update(password).digest('hex');
+const hashPassword = (password) =>
+  crypto.createHash("md5").update(password).digest("hex");
 
 const Customer = {
   create: (customerData, callback) => {
@@ -17,16 +18,32 @@ const Customer = {
     `;
 
     const valuesCustomers = [
-      customerData.client_unique_id, customerData.client_id, customerData.name, customerData.profile_picture, customerData.email,
-      customerData.email_verified_at, customerData.mobile_number, customerData.mobile_verified_at, hashPassword(customerData.password),
-      customerData.reset_password_token, customerData.login_token, customerData.token_expiry, customerData.role,
-      customerData.status || '0', new Date(), new Date(), customerData.admin_id
+      customerData.client_unique_id,
+      customerData.client_id,
+      customerData.name,
+      customerData.profile_picture,
+      customerData.email,
+      customerData.email_verified_at,
+      customerData.mobile_number,
+      customerData.mobile_verified_at,
+      hashPassword(customerData.password),
+      customerData.reset_password_token,
+      customerData.login_token,
+      customerData.token_expiry,
+      customerData.role,
+      customerData.status || "0",
+      new Date(),
+      new Date(),
+      customerData.admin_id,
     ];
 
     pool.query(sqlCustomers, valuesCustomers, (err, results) => {
       if (err) {
-        console.error('Database insertion error for customers:', err);
-        return callback({ message: 'Database insertion error for customers', error: err }, null);
+        console.error("Database insertion error for customers:", err);
+        return callback(
+          { message: "Database insertion error for customers", error: err },
+          null
+        );
       }
 
       const customerId = results.insertId;
@@ -48,27 +65,51 @@ const Customer = {
         \`agreement_document\`, \`custom_template\`, \`custom_logo\`, \`custom_address\`,
         \`status\`, \`state\`, \`state_code\`, \`additional_login\`,
         \`standard_operating_procedures\`, \`package_category\`,
-        \`service_codes\`, \`payment_contact_person\`, \`created_at\`, \`updated_at\`, 
+        \`service_codes\`, \`payment_contact_person\`, \`created_at\`, \`updated_at\`
       ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
                 ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 
-                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+                ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     `;
 
     const valuesCustomerMetas = [
-      metaData.customer_id, metaData.company_name, metaData.address, metaData.mobile_number, metaData.email,
-      metaData.email2, metaData.email3, metaData.email4, metaData.username,
-      metaData.contact_person_name, metaData.contact_person_title, metaData.escalation_point_contact,
-      metaData.single_point_of_contact, metaData.gst_number, metaData.tat_days, metaData.service_description,
-      metaData.service_fee, metaData.agreement_date, metaData.agreement_duration,
-      metaData.agreement_document, metaData.custom_template || 'no', metaData.custom_logo, metaData.custom_address,
-      metaData.status || '0', metaData.state, metaData.state_code, metaData.additional_login,
-      metaData.standard_operating_procedures, metaData.record_creation_date, metaData.package_category,
-      metaData.service_codes, metaData.payment_contact_person
+      metaData.customer_id,
+      metaData.company_name,
+      metaData.address,
+      metaData.mobile_number,
+      metaData.email,
+      metaData.email2,
+      metaData.email3,
+      metaData.email4,
+      metaData.username,
+      metaData.contact_person_name,
+      metaData.contact_person_title,
+      metaData.escalation_point_contact,
+      metaData.single_point_of_contact,
+      metaData.gst_number,
+      metaData.tat_days,
+      metaData.service_description,
+      metaData.service_fee,
+      metaData.agreement_date,
+      metaData.agreement_duration,
+      metaData.agreement_document,
+      metaData.custom_template || "no",
+      metaData.custom_logo,
+      metaData.custom_address,
+      metaData.status || "0",
+      metaData.state,
+      metaData.state_code,
+      metaData.additional_login,
+      metaData.standard_operating_procedures,
+      metaData.package_category,
+      metaData.service_codes,
+      metaData.payment_contact_person,
+      metaData.created_at,
+      metaData.updated_at,
     ];
 
     pool.query(sqlCustomerMetas, valuesCustomerMetas, (err, results) => {
       if (err) {
-        console.error('Database insertion error for customer_metas:', err);
+        console.error("Database insertion error for customer_metas:", err);
         return callback({ message: err }, null);
       }
 
@@ -85,12 +126,15 @@ const Customer = {
 
     pool.query(sql, [username, username], (err, results) => {
       if (err) {
-        console.error('Database query error:', err);
-        return callback({ message: 'Database query error', error: err }, null);
+        console.error("Database query error:", err);
+        return callback({ message: "Database query error", error: err }, null);
       }
 
       if (results.length === 0) {
-        return callback({ message: 'No customer found with the provided email or mobile' }, null);
+        return callback(
+          { message: "No customer found with the provided email or mobile" },
+          null
+        );
       }
 
       callback(null, results);
@@ -105,18 +149,25 @@ const Customer = {
       AND \`password\` = MD5(?)
     `;
 
-    pool.query(sql, [username, username, hashPassword(password)], (err, results) => {
-      if (err) {
-        console.error('Database query error:', err);
-        return callback({ message: 'Database query error', error: err }, null);
-      }
+    pool.query(
+      sql,
+      [username, username, hashPassword(password)],
+      (err, results) => {
+        if (err) {
+          console.error("Database query error:", err);
+          return callback(
+            { message: "Database query error", error: err },
+            null
+          );
+        }
 
-      if (results.length === 0) {
-        return callback({ message: 'Incorrect password or username' }, null);
-      }
+        if (results.length === 0) {
+          return callback({ message: "Incorrect password or username" }, null);
+        }
 
-      callback(null, results);
-    });
+        callback(null, results);
+      }
+    );
   },
 
   updateToken: (id, token, tokenExpiry, callback) => {
@@ -128,12 +179,18 @@ const Customer = {
 
     pool.query(sql, [token, tokenExpiry, id], (err, results) => {
       if (err) {
-        console.error('Database query error:', err);
-        return callback({ message: 'Database update error', error: err }, null);
+        console.error("Database query error:", err);
+        return callback({ message: "Database update error", error: err }, null);
       }
 
       if (results.affectedRows === 0) {
-        return callback({ message: 'Token update failed. Customer not found or no changes made.' }, null);
+        return callback(
+          {
+            message:
+              "Token update failed. Customer not found or no changes made.",
+          },
+          null
+        );
       }
 
       callback(null, results);
@@ -149,12 +206,12 @@ const Customer = {
 
     pool.query(sql, [id], (err, results) => {
       if (err) {
-        console.error('Database query error:', err);
-        return callback({ message: 'Database query error', error: err }, null);
+        console.error("Database query error:", err);
+        return callback({ message: "Database query error", error: err }, null);
       }
 
       if (results.length === 0) {
-        return callback({ message: 'Customer not found' }, null);
+        return callback({ message: "Customer not found" }, null);
       }
 
       callback(null, results);
@@ -170,17 +227,23 @@ const Customer = {
 
     pool.query(sql, [id], (err, results) => {
       if (err) {
-        console.error('Database query error:', err);
-        return callback({ message: 'Database update error', error: err }, null);
+        console.error("Database query error:", err);
+        return callback({ message: "Database update error", error: err }, null);
       }
 
       if (results.affectedRows === 0) {
-        return callback({ message: 'Token clear failed. Customer not found or no changes made.' }, null);
+        return callback(
+          {
+            message:
+              "Token clear failed. Customer not found or no changes made.",
+          },
+          null
+        );
       }
 
       callback(null, results);
     });
-  }
+  },
 };
 
 module.exports = Customer;
