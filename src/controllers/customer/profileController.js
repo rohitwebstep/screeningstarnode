169e2c414fd0bcd9391afbe29b825104a9b5d3cf
rohitwebstep <rohitwebstep@gmail.com>
2024-08-27 13:22:45 +0530

    message for duplicate entries

--- src/controllers/customer/profileController.js
+++ src/controllers/customer/profileController.js
@@ -102,6 +102,7 @@ exports.create = (req, res) => {
       message: `Missing required fields: ${missingFields.join(", ")}`,
     });
   }
+
   const action = JSON.stringify({ customer: "create" });
   AdminCommon.isAdminAuthorizedForAction(admin_id, action, (result) => {
     if (!result.status) {
@@ -125,183 +126,226 @@ exports.create = (req, res) => {
       const newToken = result.newToken;
       const password = generatePassword(company_name);
 
-      // Create new customer record
-      Customer.create(
-        {
-          admin_id,
-          client_unique_id: client_code,
-          name: company_name,
-          address,
-          profile_picture: null,
-          emails_json: JSON.stringify(emails),
-          mobile_number,
-          role,
-          status: "0",
-          additional_login: additional_login_int,
-          username:
-            additional_login && additional_login.toLowerCase() === "yes"
-              ? username
-              : null,
-        },
-        (err, result) => {
-          if (err) {
-            console.error("Database error while creating customer:", err);
-            AdminCommon.adminActivityLog(
-              admin_id,
-              "Customer",
-              "Create",
-              "0",
-              null,
-              err.message,
-              () => {}
-            );
-            return res
-              .status(500)
-              .json({ status: false, message: err.message });
-          }
+      // Check if client_unique_id already exists
+      Customer.checkUniqueId(client_code, (err, exists) => {
+        if (err) {
+          console.error("Error checking unique ID:", err);
+          return res
+            .status(500)
+            .json({ status: false, message: "Internal server error" });
+        }
 
-          console.log("Customer created successfully. ID:", result.insertId);
-          const customerId = result.insertId;
+        if (exists) {
+          return res.status(400).json({
+            status: false,
+            message: `Client Unique ID '${client_code}' already exists.`,
+          });
+        }
 
-          Customer.createCustomerMeta(
-            {
-              customer_id: customerId,
-              address,
-              contact_person_name: contact_person,
-              escalation_point_contact: name_of_escalation,
-              single_point_of_contact: client_spoc,
-              gst_number: gstin,
-              tat_days: tat,
-              agreement_date: date_agreement,
-              agreement_duration: agreement_period,
-              agreement_document,
-              custom_template,
-              custom_logo:
-                custom_template && custom_template.toLowerCase() === "yes"
-                  ? custom_logo
-                  : null,
-              custom_address:
-                custom_template && custom_template.toLowerCase() === "yes"
-                  ? custom_address
-                  : null,
-              state,
-              state_code,
-              payment_contact_person: null,
-            },
-            (err, metaResult) => {
-              if (err) {
-                console.error(
-                  "Database error while creating customer meta:",
-                  err
-                );
-                AdminCommon.adminActivityLog(
-                  admin_id,
-                  "Customer Meta",
-                  "Create",
-                  "0",
-                  `{id: ${customerId}}`,
-                  err.message,
-                  () => {}
-                );
-                return res.status(500).json({
-                  status: false,
-                  message: err.error,
-                });
-              }
+        // Check if username is required and exists
+        if (additional_login && additional_login.toLowerCase() === "yes") {
+          Customer.checkUsername(username, (err, exists) => {
+            if (err) {
+              console.error("Error checking username:", err);
+              return res
+                .status(500)
+                .json({ status: false, message: "Internal server error" });
+            }
 
-              console.log("Customer meta created successfully.");
+            if (exists) {
+              return res.status(400).json({
+                status: false,
+                message: `Username '${username}' already exists.`,
+              });
+            }
 
-              // Iterate over branches array to create branch records
-              const branchCreationPromises = branches.map(
-                (branch, index) =>
-                  new Promise((resolve, reject) => {
-                    Branch.create(
-                      {
-                        customer_id: customerId,
-                        name: branch.branch_name,
-                        email: branch.branch_email,
-                        head: index === 0 ? 1 : 0, // Set head to 1 for the first branch, 0 for others
-                      },
-                      (err, branchResult) => {
-                        if (err) {
-                          console.error(
-                            "Error creating branch:",
-                            branch.branch_name,
-                            err
-                          );
-                          return reject(err);
-                        }
-                        console.log(
-                          "Branch created successfully:",
-                          branch.branch_name
-                        );
-                        resolve(branchResult);
-                      }
-                    );
-                  })
+            // Create new customer record
+            createCustomerRecord();
+          });
+        } else {
+          // Create new customer record
+          createCustomerRecord();
+        }
+      });
+
+      function createCustomerRecord() {
+        Customer.create(
+          {
+            admin_id,
+            client_unique_id: client_code,
+            name: company_name,
+            address,
+            profile_picture: null,
+            emails_json: JSON.stringify(emails),
+            mobile_number,
+            role,
+            status: "0",
+            additional_login: additional_login_int,
+            username:
+              additional_login && additional_login.toLowerCase() === "yes"
+                ? username
+                : null,
+          },
+          (err, result) => {
+            if (err) {
+              console.error("Database error while creating customer:", err);
+              AdminCommon.adminActivityLog(
+                admin_id,
+                "Customer",
+                "Create",
+                "0",
+                null,
+                err.message,
+                () => {}
               );
+              return res
+                .status(500)
+                .json({ status: false, message: err.message });
+            }
 
-              Promise.all(branchCreationPromises)
-                .then((branchResults) => {
-                  console.log("All branches created successfully.");
+            console.log("Customer created successfully. ID:", result.insertId);
+            const customerId = result.insertId;
+
+            Customer.createCustomerMeta(
+              {
+                customer_id: customerId,
+                address,
+                contact_person_name: contact_person,
+                escalation_point_contact: name_of_escalation,
+                single_point_of_contact: client_spoc,
+                gst_number: gstin,
+                tat_days: tat,
+                agreement_date: date_agreement,
+                agreement_duration: agreement_period,
+                agreement_document,
+                custom_template,
+                custom_logo:
+                  custom_template && custom_template.toLowerCase() === "yes"
+                    ? custom_logo
+                    : null,
+                custom_address:
+                  custom_template && custom_template.toLowerCase() === "yes"
+                    ? custom_address
+                    : null,
+                state,
+                state_code,
+                payment_contact_person: null,
+              },
+              (err, metaResult) => {
+                if (err) {
+                  console.error(
+                    "Database error while creating customer meta:",
+                    err
+                  );
                   AdminCommon.adminActivityLog(
                     admin_id,
-                    "Customer",
+                    "Customer Meta",
                     "Create",
-                    "1",
+                    "0",
                     `{id: ${customerId}}`,
-                    null,
+                    err.message,
                     () => {}
                   );
-
-                  // Send email notification
-                  sendEmail(
-                    "customer",
-                    "create",
-                    emails,
-                    company_name,
-                    password
-                  )
-                    .then(() => {
-                      console.log("Email sent successfully.");
-                      res.json({
-                        status: true,
-                        message:
-                          "Customer and branches created successfully, and credentials sent through mail.",
-                        data: {
-                          customer: result,
-                          meta: metaResult,
-                          branches: branchResults,
-                        },
-                        _token: newToken,
-                      });
-                    })
-                    .catch((emailError) => {
-                      console.error("Error sending email:", emailError);
-                      res.json({
-                        status: true,
-                        message:
-                          "Customer and branches created successfully, but failed to send email.",
-                        data: {
-                          customer: result,
-                          meta: metaResult,
-                          branches: branchResults,
-                        },
-                        _token: newToken,
-                      });
-                    });
-                })
-                .catch((branchError) => {
-                  console.error("Error creating branches:", branchError);
-                  res.status(500).json({
+                  return res.status(500).json({
                     status: false,
-                    message: branchError,
+                    message: err.error,
                   });
-                });
-            }
-          );
-        }
-      );
+                }
+
+                console.log("Customer meta created successfully.");
+
+                // Iterate over branches array to create branch records
+                const branchCreationPromises = branches.map(
+                  (branch, index) =>
+                    new Promise((resolve, reject) => {
+                      Branch.create(
+                        {
+                          customer_id: customerId,
+                          name: branch.branch_name,
+                          email: branch.branch_email,
+                          head: index === 0 ? 1 : 0, // Set head to 1 for the first branch, 0 for others
+                        },
+                        (err, branchResult) => {
+                          if (err) {
+                            console.error(
+                              "Error creating branch:",
+                              branch.branch_name,
+                              err
+                            );
+                            return reject(err);
+                          }
+                          console.log(
+                            "Branch created successfully:",
+                            branch.branch_name
+                          );
+                          resolve(branchResult);
+                        }
+                      );
+                    })
+                );
+
+                Promise.all(branchCreationPromises)
+                  .then((branchResults) => {
+                    console.log("All branches created successfully.");
+                    AdminCommon.adminActivityLog(
+                      admin_id,
+                      "Customer",
+                      "Create",
+                      "1",
+                      `{id: ${customerId}}`,
+                      null,
+                      () => {}
+                    );
+
+                    // Send email notification
+                    sendEmail(
+                      "customer",
+                      "create",
+                      emails,
+                      company_name,
+                      password
+                    )
+                      .then(() => {
+                        console.log("Email sent successfully.");
+                        res.json({
+                          status: true,
+                          message:
+                            "Customer and branches created successfully, and credentials sent through mail.",
+                          data: {
+                            customer: result,
+                            meta: metaResult,
+                            branches: branchResults,
+                          },
+                          _token: newToken,
+                        });
+                      })
+                      .catch((emailError) => {
+                        console.error("Error sending email:", emailError);
+                        res.json({
+                          status: true,
+                          message:
+                            "Customer and branches created successfully, but failed to send email.",
+                          data: {
+                            customer: result,
+                            meta: metaResult,
+                            branches: branchResults,
+                          },
+                          _token: newToken,
+                        });
+                      });
+                  })
+                  .catch((branchError) => {
+                    console.error("Error creating branches:", branchError);
+                    res.status(500).json({
+                      status: false,
+                      message: branchError,
+                    });
+                  });
+              }
+            );
+          }
+        );
+      }
     });
   });
 };
