6fd16f89844a7b11c7bd61614bf4f67659c3ab72
rohitwebstep <rohitwebstep@gmail.com>
2024-08-29 17:09:49 +0530

    refine the files

--- src/routes/customer/indexRoutes.js
+++ src/routes/customer/indexRoutes.js
@@ -1,16 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const authController = require("../../controllers/customer/authController");
 const profileController = require("../../controllers/customer/profileController");
-
-// Authentication routes
-router.post("/login", authController.login);
-router.get("/logout", authController.logout);
-router.post("/verify-admin-login", authController.validateLogin);
 
 // Profile routes
 router.post("/create", profileController.create);
 router.get("/list", profileController.list);
-router.delete('/delete', profileController.delete);
+router.delete("/delete", profileController.delete);
 
 module.exports = router;
