7e16a45da71264424cba9287bc71e1f944653d4b
rohitwebstep <rohitwebstep@gmail.com>
2024-08-29 16:01:14 +0530

    branch login

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const branchController = require("../../../controllers/customer/branch/branchController");
+const authController = require("../../../controllers/customer/branch/authController");
 
 // Basic routes
-router.post("/create", branchController.create);
+router.post('/login', authController.login);
 
 module.exports = router;
