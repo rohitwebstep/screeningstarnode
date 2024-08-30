1d7f042a3b513c79fd181db5c3fa78305d41a555
rohitwebstep <rohitwebstep@gmail.com>
2024-08-30 13:51:30 +0530

    create branch listing

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../../../controllers/customer/branch/authController");
+const profileController = require("../../../controllers/customer/branch/profileController");
 const clientRoutes = require("./clientRoutes");
 
 // Basic routes
 router.post("/login", authController.login);
+router.get("/list", profileController.list);
 router.get("/logout", authController.logout);
 
 router.use("/client-application", clientRoutes);
