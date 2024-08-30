086df4549d49ee506f79685a96e5cbf1f14288ad
rohitwebstep <rohitwebstep@gmail.com>
2024-08-30 12:15:09 +0530

    refine branch model

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const branchController = require("../../../controllers/customer/branch/branchController");
 const authController = require("../../../controllers/customer/branch/authController");
 const clientRoutes = require("./clientRoutes");
 
