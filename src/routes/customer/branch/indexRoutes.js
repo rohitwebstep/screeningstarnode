148f2d9a02391e139ba971e2bb1fd32c2b9eda01
rohitwebstep <rohitwebstep@gmail.com>
2024-08-30 12:07:02 +0530

    client application create

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 const router = express.Router();
 const branchController = require("../../../controllers/customer/branch/branchController");
 const authController = require("../../../controllers/customer/branch/authController");
+const clientRoutes = require("./clientRoutes");
 
 // Basic routes
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 
+router.use("/client-application", clientRoutes);
 module.exports = router;
