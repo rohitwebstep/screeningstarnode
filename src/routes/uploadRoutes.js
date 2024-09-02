cde228b144b373593c132d958995bfb3bd5d97ce
rohitwebstep <rohitwebstep@gmail.com>
2024-09-02 17:11:50 +0530

    trying to shwo image

--- src/routes/uploadRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-
-// Serve static files from the 'uploads/customers' directory
-app.use(
-  "/customers",
-  express.static(path.join(__dirname, "uploads/customers"))
-);
