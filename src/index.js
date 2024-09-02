9fd4ad610037087556aabee178161c208ec5324b
rohitwebstep <rohitwebstep@gmail.com>
2024-09-02 17:24:41 +0530

    add image show route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const adminRoutes = require("./routes/admin/indexRoutes");
 const customerRoutes = require("./routes/customer/indexRoutes");
 const branchRoutes = require("./routes/customer/branch/indexRoutes");
@@ -20,6 +21,11 @@ app.use("/customer", customerRoutes);
 app.use("/branch", branchRoutes);
 app.use("/package", packageRoutes);
 app.use("/service", serviceRoutes);
+
+app.use(
+  "/uploads/customers",
+  express.static(path.join(__dirname, "uploads/customers"))
+);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
