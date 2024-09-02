89ef2c0e8757950885a6179fb71da80e748dcf16
rohitwebstep <rohitwebstep@gmail.com>
2024-09-02 10:26:32 +0530

    error fix

--- src/routes/customer/branch/clientRoutes.js
+++ src/routes/customer/branch/clientRoutes.js
@@ -5,7 +5,7 @@ const clientController = require("../../../controllers/customer/branch/client/ap
 // Basic routes
 router.post("/create", clientController.create);
 router.get("/list", clientController.list);
-router.put("/update", packageController.update);
+router.put("/update", clientController.update);
 router.delete("/delete", clientController.delete);
 
 module.exports = router;
