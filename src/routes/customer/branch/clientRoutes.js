d7fed7114b532cd72aa7981a35402f279ff68862
rohitwebstep <rohitwebstep@gmail.com>
2024-09-02 10:22:11 +0530

    client application add

--- src/routes/customer/branch/clientRoutes.js
+++ src/routes/customer/branch/clientRoutes.js
@@ -5,6 +5,7 @@ const clientController = require("../../../controllers/customer/branch/client/ap
 // Basic routes
 router.post("/create", clientController.create);
 router.get("/list", clientController.list);
+router.put("/update", packageController.update);
 router.delete("/delete", clientController.delete);
 
 module.exports = router;
