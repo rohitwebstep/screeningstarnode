52324fb5336710c002ef867ea7f070dcc9bff20b
rohitwebstep <rohitwebstep@gmail.com>
2024-09-02 16:59:17 +0530

    remove upload routes

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const customerRoutes = require("./routes/customer/indexRoutes");
 const branchRoutes = require("./routes/customer/branch/indexRoutes");
 const packageRoutes = require("./routes/admin/packageRoutes");
 const serviceRoutes = require("./routes/admin/serviceRoutes");
-const uploadRoutes = require("./routes/uploadRoutes");
-
 require("dotenv").config(); // Ensure you load environment variables
 
 const app = express();
@@ -21,8 +19,6 @@ app.use("/customer", customerRoutes);
 app.use("/branch", branchRoutes);
 app.use("/package", packageRoutes);
 app.use("/service", serviceRoutes);
-
-app.use("/uploads", uploadRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
