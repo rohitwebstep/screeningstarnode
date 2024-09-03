ffb3db8bc3a3cd646a65bc101d576ed998a8f5ad
rohitwebstep <rohitwebstep@gmail.com>
2024-09-03 11:57:04 +0530

    route for customer info with branch auth

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../../../controllers/customer/branch/authController");
 const profileController = require("../../../controllers/customer/branch/profileController");
+const customerController = require("../../../controllers/customer/profileController");
 const clientRoutes = require("./clientRoutes");
 
 // Basic routes
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/list", profileController.list);
+router.get("/customer-info", customerController.customerBasicInfoWithBranchAuth);
 router.get("/list-by-customer", profileController.listByCustomerID);
 router.put("/update", profileController.update);
 router.delete("/delete", profileController.delete);
