8ee56ad9a338b7d2194cb76f0ac33d45b08fc967
rohitwebstep <rohitwebstep@gmail.com>
2024-09-03 17:15:51 +0530

    default permissions for branches

--- src/models/customer/branch/branchModel.js
+++ src/models/customer/branch/branchModel.js
@@ -4,16 +4,17 @@ const Branch = {
   create: (BranchData, callback) => {
     const sqlBranch = `
       INSERT INTO \`branches\` (
-        \`customer_id\`, \`name\`, \`email\`, \`is_head\`, \`password\`
-      ) VALUES (?, ?, ?, ?, MD5(?))
+        \`customer_id\`, \`name\`, \`email\`, \`is_head\`, \`password\`, \`permissions\`
+      ) VALUES (?, ?, ?, ?, MD5(?), ?)
     `;
-
+    const permissions = `{"client_application": { "create": true, "update": true, "view": true, "delete": true },"candidate_application": { "create": true, "update": true, "view": true, "delete": true }}`;
     const valuesBranch = [
       BranchData.customer_id,
       BranchData.name,
       BranchData.email,
       BranchData.head,
       BranchData.password,
+      permissions,
     ];
 
     pool.query(sqlBranch, valuesBranch, (err, results) => {
