b056c373acca8acb6ccac0cc7ecb0507a2162ad2
rohitwebstep <rohitwebstep@gmail.com>
2024-09-04 12:51:58 +0530

    create customer application

--- src/routes/customer/branch/indexRoutes.js
+++ src/routes/customer/branch/indexRoutes.js
@@ -4,16 +4,21 @@ const authController = require("../../../controllers/customer/branch/authControl
 const profileController = require("../../../controllers/customer/branch/profileController");
 const customerController = require("../../../controllers/customer/profileController");
 const clientRoutes = require("./clientRoutes");
+const customerRoutes = require("./customerRoutes");
 
 // Basic routes
 router.post("/login", authController.login);
 router.get("/logout", authController.logout);
 router.get("/list", profileController.list);
 router.get("/is-email-used", profileController.isEmailUsed);
-router.get("/customer-info",customerController.customerBasicInfoWithBranchAuth);
+router.get(
+  "/customer-info",
+  customerController.customerBasicInfoWithBranchAuth
+);
 router.get("/list-by-customer", profileController.listByCustomerID);
 router.put("/update", profileController.update);
 router.delete("/delete", profileController.delete);
 
 router.use("/client-application", clientRoutes);
+router.use("/customer-application", customerRoutes);
 module.exports = router;
