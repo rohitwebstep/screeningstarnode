794fc51f48969e2165111fa0d957f425256e6b7b
rohitwebstep <rohitwebstep@gmail.com>
2024-09-25 10:53:53 +0530

    created fetch password by branch email

--- src/routes/customer/indexRoutes.js
+++ src/routes/customer/indexRoutes.js
@@ -7,6 +7,7 @@ router.post("/create", profileController.create);
 router.get("/list", profileController.list);
 router.get("/inactive-list", profileController.inactiveList);
 router.put("/update", profileController.update);
+router.get("/fetch-branch-password", profileController.fetchBranchPassword);
 router.get("/active", profileController.active);
 router.get("/inactive", profileController.inactive);
 router.delete("/delete", profileController.delete);
