3511157cd0c1bb57724c545ede015b8e8870b485
rohitwebstep <rohitwebstep@gmail.com>
2024-10-01 11:13:27 +0530

    cmt update

--- src/routes/admin/clientMasterTrackerRoutes.js
+++ src/routes/admin/clientMasterTrackerRoutes.js
@@ -7,6 +7,7 @@ router.get('/list', clientMasterTrackerController.list);
 router.get('/branch-list-by-customer', clientMasterTrackerController.listByCustomerId);
 router.get('/applications-by-branch', clientMasterTrackerController.applicationListByBranch);
 router.get('/application-by-id', clientMasterTrackerController.applicationByID);
+router.put("/update", clientMasterTrackerController.update);
 router.get('/report-form-json-by-service-id', clientMasterTrackerController.reportFormJsonByServiceID);
 
 module.exports = router;
