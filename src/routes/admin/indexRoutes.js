4fcef8dfda6f9413af00fa0f86b657b63866890f
rohitwebstep <rohitwebstep@gmail.com>
2024-10-03 15:52:43 +0530

    created update password

--- src/routes/admin/indexRoutes.js
+++ src/routes/admin/indexRoutes.js
@@ -1,10 +1,11 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../../controllers/admin/authController');
+const authController = require("../../controllers/admin/authController");
 
 // Authentication routes
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
-router.post('/verify-admin-login', authController.validateLogin);
+router.post("/login", authController.login);
+router.put("/password-update", authController.passwordUpdate);
+router.get("/logout", authController.logout);
+router.post("/verify-admin-login", authController.validateLogin);
 
 module.exports = router;
