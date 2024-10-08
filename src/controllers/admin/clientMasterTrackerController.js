d324ff211c4956f9fcc12145a2ddc9e8519f615d
rohitwebstep <rohitwebstep@gmail.com>
2024-10-08 15:21:45 +0530

    craete customerBasicInfoWithAdminAuth

--- src/controllers/admin/clientMasterTrackerController.js
+++ src/controllers/admin/clientMasterTrackerController.js
@@ -1177,3 +1177,63 @@ exports.generateReport = (req, res) => {
     });
   });
 };
+
+exports.customerBasicInfoWithAdminAuth = (req, res) => {
+  const { customer_id, admin_id, _token } = req.query;
+
+  let missingFields = [];
+  if (!customer_id || customer_id === "" || customer_id === undefined)
+    missingFields.push("Customer ID");
+  if (!admin_id || admin_id === "" || admin_id === undefined)
+    missingFields.push("Admin ID");
+  if (!_token || _token === "" || _token === undefined)
+    missingFields.push("Token");
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      status: false,
+      message: `Missing required fields: ${missingFields.join(", ")}`,
+    });
+  }
+
+  const action = JSON.stringify({ cmt_application: "view" });
+  AdminCommon.isAdminAuthorizedForAction(admin_id, action, (result) => {
+    if (!result.status) {
+      return res.status(403).json({
+        status: false,
+        message: result.message, // Return the message from the authorization function
+      });
+    }
+
+    // Verify admin token
+    AdminCommon.isAdminTokenValid(_token, admin_id, (err, result) => {
+      if (err) {
+        console.error("Error checking token validity:", err);
+        return res.status(500).json({ status: false, message: err.message });
+      }
+
+      if (!result.status) {
+        return res.status(401).json({ status: false, message: result.message });
+      }
+
+      const newToken = result.newToken;
+
+      Customer.basicInfoByID(customer_id, (err, result) => {
+        if (err) {
+          console.error("Database error:", err);
+          return res
+            .status(500)
+            .json({ status: false, message: err.message, token: newToken });
+        }
+
+        res.json({
+          status: true,
+          message: "Customer Info fetched successfully",
+          customers: result,
+          totalResults: result.length,
+          token: newToken,
+        });
+      });
+    });
+  });
+};
