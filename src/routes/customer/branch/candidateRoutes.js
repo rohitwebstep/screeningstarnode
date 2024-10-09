1e9639870cc3532d0bce94811d2e1298424181e1
rohitwebstep <rohitwebstep@gmail.com>
2024-10-09 13:08:39 +0530

    create cef controller,model and formJson Method

--- src/routes/customer/branch/candidateRoutes.js
+++ src/routes/customer/branch/candidateRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const candidateController = require("../../../controllers/customer/branch/candidate/applicationController");
+const cefRoutes = require("./candidate-email-form/cefRoutes.js");
 
 // Basic routes
 router.post("/create", candidateController.create);
 router.get("/list", candidateController.list);
 router.put("/update", candidateController.update);
 router.delete("/delete", candidateController.delete);
+router.delete("/", candidateController.delete);
+
+app.use("/candidate-email-form", cefRoutes);
 
 module.exports = router;
