14383453b3ee2cc5d653c1a809848856e466828c
rohitwebstep <rohitwebstep@gmail.com>
2024-10-11 10:58:24 +0530

    create annexure by service id in branch login

--- src/controllers/customer/branch/profileController.js
+++ src/controllers/customer/branch/profileController.js
@@ -3,6 +3,7 @@ const Branch = require("../../../models/customer/branch/branchModel");
 const BranchCommon = require("../../../models/customer/branch/commonModel");
 const AdminCommon = require("../../../models/admin/commonModel");
 const Service = require("../../../models/admin/serviceModel");
+const clientMasterTracker = require("../../../models/admin/clientMasterTrackerModel");
 
 const generatePassword = (companyName) => {
   const firstName = companyName.split(" ")[0];
@@ -836,6 +837,142 @@ exports.getServiceById = (req, res) => {
             token: newToken,
           });
         });
+      }
+    );
+  });
+};
+
+exports.annexureDataByServiceId = (req, res) => {
+  const { service_id, application_id, branch_id, _token } = req.query;
+
+  let missingFields = [];
+  if (
+    !service_id ||
+    service_id === "" ||
+    service_id === undefined ||
+    service_id === "undefined"
+  ) {
+    missingFields.push("Service ID");
+  }
+
+  if (
+    !application_id ||
+    application_id === "" ||
+    application_id === undefined ||
+    application_id === "undefined"
+  ) {
+    missingFields.push("Application ID");
+  }
+
+  if (
+    !branch_id ||
+    branch_id === "" ||
+    branch_id === undefined ||
+    branch_id === "undefined"
+  ) {
+    missingFields.push("Admin ID");
+  }
+  if (
+    !_token ||
+    _token === "" ||
+    _token === undefined ||
+    _token === "undefined"
+  ) {
+    missingFields.push("Token");
+  }
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      status: false,
+      message: `Missing required fields: ${missingFields.join(", ")}`,
+    });
+  }
+
+  const action = JSON.stringify({ service: "view" });
+  BranchCommon.isBranchAuthorizedForAction(branch_id, action, (authResult) => {
+    if (!authResult.status) {
+      return res.status(403).json({
+        status: false,
+        message: authResult.message, // Return the authorization error message
+      });
+    }
+
+    // Step 3: Verify the branch token
+    BranchCommon.isBranchTokenValid(
+      _token,
+      branch_id,
+      (tokenErr, tokenResult) => {
+        if (tokenErr) {
+          console.error("Error checking token validity:", tokenErr);
+          return res.status(500).json({
+            status: false,
+            message: "Server error while verifying token.",
+          });
+        }
+
+        if (!tokenResult.status) {
+          return res.status(401).json({
+            status: false,
+            message: tokenResult.message, // Return the token validation message
+          });
+        }
+
+        const newToken = tokenResult.newToken;
+
+        clientMasterTracker.reportFormJsonByServiceID(
+          service_id,
+          (err, reportFormJson) => {
+            if (err) {
+              console.error("Error fetching report form JSON:", err);
+              return res
+                .status(500)
+                .json({ status: false, message: err.message, token: newToken });
+            }
+
+            if (!reportFormJson) {
+              return res.status(404).json({
+                status: false,
+                message: "Report form JSON not found",
+                token: newToken,
+              });
+            }
+
+            const parsedData = JSON.parse(reportFormJson.json);
+            const db_table = parsedData.db_table;
+            const modifiedDbTable = db_table.replace(/-/g, "_");
+
+            clientMasterTracker.annexureData(
+              application_id,
+              modifiedDbTable,
+              (err, annexureData) => {
+                if (err) {
+                  console.error("Database error:", err);
+                  return res.status(500).json({
+                    status: false,
+                    message: "An error occurred while fetching annexure data.",
+                    error: err.message,
+                    token: newToken,
+                  });
+                }
+
+                if (!annexureData) {
+                  return res.status(404).json({
+                    status: false,
+                    message: "Annexure Data not found.",
+                    token: newToken,
+                  });
+                }
+
+                res.status(200).json({
+                  status: true,
+                  message: "Application fetched successfully.",
+                  annexureData,
+                  token: newToken,
+                });
+              }
+            );
+          }
+        );
       }
     );
   });
