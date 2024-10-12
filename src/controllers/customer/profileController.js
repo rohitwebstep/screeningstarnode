947b1e217d8809eb4640931e330e0d55475417f9
rohitwebstep <rohitwebstep@gmail.com>
2024-10-12 11:00:06 +0530

    added image upload

--- src/controllers/customer/profileController.js
+++ src/controllers/customer/profileController.js
@@ -4,6 +4,9 @@ const Branch = require("../../models/customer/branch/branchModel");
 const AdminCommon = require("../../models/admin/commonModel");
 const BranchCommon = require("../../models/customer/branch/commonModel");
 const { createMail } = require("../../mailer/customer/createMail");
+const fs = require("fs");
+const path = require("path");
+const { upload, saveImage, saveImages } = require("../../utils/imageSave");
 
 // Helper function to generate a password
 const generatePassword = (companyName) => {
@@ -89,6 +92,61 @@ exports.create = (req, res) => {
     });
   }
 
+  if (req.files.images) {
+    const targetDir = "uploads/customer/";
+    fs.mkdir(targetDir, { recursive: true }, (err) => {
+      if (err) {
+        console.error("Error creating directory:", err);
+        return res.status(500).json({
+          status: false,
+          message: "Error creating directory.",
+        });
+      }
+      // Use multer to handle the upload
+      upload(req, res, async (err) => {
+        if (err) {
+          return res.status(400).json({
+            status: false,
+            message: "Error uploading file.",
+          });
+        }
+
+        try {
+          let savedImagePaths = [];
+
+          // Check if multiple files are uploaded under the "images" field
+          if (req.files.images) {
+            savedImagePaths = await saveImages(req.files.images, targetDir); // Pass targetDir to saveImages
+          }
+
+          // Check if a single file is uploaded under the "image" field
+          if (req.files.image && req.files.image.length > 0) {
+            const savedImagePath = await saveImage(
+              req.files.image[0],
+              targetDir
+            ); // Pass targetDir to saveImage
+            savedImagePaths.push(savedImagePath);
+          }
+
+          // Return success response
+          return res.status(201).json({
+            status: true,
+            message:
+              savedImagePaths.length > 0
+                ? "Image(s) saved successfully"
+                : "No images uploaded",
+            data: savedImagePaths,
+          });
+        } catch (error) {
+          console.error("Error saving image:", error);
+          return res.status(500).json({
+            status: false,
+            message: "An error occurred while saving the image",
+          });
+        }
+      });
+    });
+  }
   const action = JSON.stringify({ customer: "create" });
   AdminCommon.isAdminAuthorizedForAction(admin_id, action, (result) => {
     if (!result.status) {
