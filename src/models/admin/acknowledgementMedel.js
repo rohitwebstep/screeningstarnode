d3de69d8479170499ca3959e6e0378bb13675a6c
rohitwebstep <rohitwebstep@gmail.com>
2024-10-15 16:42:28 +0530

    create Acknowledgement

---
+++ src/models/admin/acknowledgementMedel.js
@@ -0,0 +1,63 @@
+const crypto = require("crypto");
+const Acknowledgement = require("../../models/admin/acknowledgementModel");
+const AdminCommon = require("../../models/admin/commonModel");
+
+// Controller to list all customers
+exports.list = (req, res) => {
+  const { admin_id, _token } = req.query;
+
+  let missingFields = [];
+  if (!admin_id || admin_id === "") missingFields.push("Admin ID");
+  if (!_token || _token === "") missingFields.push("Token");
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      status: false,
+      message: `Missing required fields: ${missingFields.join(", ")}`,
+    });
+  }
+
+  const action = JSON.stringify({ acknowledgement: "view" });
+  AdminCommon.isAdminAuthorizedForAction(admin_id, action, (authResult) => {
+    if (!authResult.status) {
+      return res.status(403).json({
+        status: false,
+        message: authResult.message, // Return the message from the authorization function
+      });
+    }
+
+    // Verify admin token
+    AdminCommon.isAdminTokenValid(_token, admin_id, (err, tokenResult) => {
+      if (err) {
+        console.error("Error checking token validity:", err);
+        return res.status(500).json({ status: false, message: err.message });
+      }
+
+      if (!tokenResult.status) {
+        return res
+          .status(401)
+          .json({ status: false, message: tokenResult.message });
+      }
+
+      const newToken = tokenResult.newToken;
+
+      // Fetch customers from Acknowledgement model
+      Acknowledgement.list((err, customers) => {
+        if (err) {
+          console.error("Database error:", err);
+          return res
+            .status(500)
+            .json({ status: false, message: err.message, token: newToken });
+        }
+
+        res.json({
+          status: true,
+          message: "Customers fetched successfully",
+          customers: customers,
+          totalResults: customers.length,
+          token: newToken,
+        });
+      });
+    });
+  });
+};
