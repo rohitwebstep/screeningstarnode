f67a78eb210830a5dc6bb77d5e747910fe967113
rohitwebstep <rohitwebstep@gmail.com>
2024-10-25 18:02:42 +0530

    undo db code

--- src/config/db.js
+++ src/config/db.js
@@ -7,17 +7,6 @@ console.log("DB_USER:", process.env.DB_USER);
 console.log("DB_NAME:", process.env.DB_NAME);
 
 // Create a connection pool
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-//   connectTimeout: 120000, // 2 minutes for individual connection attempts
-// });
-
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -26,14 +15,8 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
-  connectTimeout: 120000,
-  // Add force IPv4
-  multipleStatements: true,
-  connectAttributes: {
-    ipv4: true
-  },
+  connectTimeout: 120000, // 2 minutes for individual connection attempts
 });
-
 
 // Function to start a connection
 const startConnection = (callback) => {
