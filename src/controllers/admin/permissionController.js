2033142d07460bc3d61083f4045fcc18a22efc79
yashika181007 <yashikawebstep@gmail.com>
2024-11-28 10:20:25 +0530

    permission listing

--- src/controllers/admin/permissionController.js
+++ src/controllers/admin/permissionController.js
@@ -15,7 +15,7 @@ exports.rolesList = (req, res) => {
       message: `Missing required fields: ${missingFields.join(", ")}`,
     });
   }
-  const action = JSON.stringify({ service: "view" });
+  const action = JSON.stringify({ admin: "create" });
   Common.isAdminAuthorizedForAction(admin_id, action, (result) => {
     if (!result.status) {
       return res.status(403).json({
@@ -36,6 +36,59 @@ exports.rolesList = (req, res) => {
       const newToken = result.newToken;
 
       Permission.rolesList((err, result) => {
+        if (err) {
+          console.error("Database error:", err);
+          return res
+            .status(500)
+            .json({ status: false, message: err.message, token: newToken });
+        }
+
+        res.json({
+          status: true,
+          message: "Roles fetched successfully",
+          roles: result,
+          totalResults: result.length,
+          token: newToken,
+        });
+      });
+    });
+  });
+};
+
+exports.list = (req, res) => {
+  const { admin_id, _token } = req.query;
+
+  let missingFields = [];
+  if (!admin_id || admin_id === "") missingFields.push("Admin ID");
+  if (!_token || _token === "") missingFields.push("Token");
+
+  if (missingFields.length > 0) {
+    return res.status(400).json({
+      status: false,
+      message: `Missing required fields: ${missingFields.join(", ")}`,
+    });
+  }
+  const action = JSON.stringify({ permission: "view" });
+  Common.isAdminAuthorizedForAction(admin_id, action, (result) => {
+    if (!result.status) {
+      return res.status(403).json({
+        status: false,
+        message: result.message, // Return the message from the authorization function
+      });
+    }
+    Common.isAdminTokenValid(_token, admin_id, (err, result) => {
+      if (err) {
+        console.error("Error checking token validity:", err);
+        return res.status(500).json(err);
+      }
+
+      if (!result.status) {
+        return res.status(401).json({ status: false, message: result.message });
+      }
+
+      const newToken = result.newToken;
+
+      Permission.list((err, result) => {
         if (err) {
           console.error("Database error:", err);
           return res
--- src/models/admin/permissionModel.js
+++ src/models/admin/permissionModel.js
@@ -25,6 +25,32 @@ const Permission = {
       });
     });
   },
+
+  list: (callback) => {
+    const sql = `
+      SELECT 
+        role,
+        json
+      FROM \`permissions\`
+    `;
+
+    startConnection((err, connection) => {
+      if (err) {
+        return callback(err, null);
+      }
+
+      connection.query(sql, (queryErr, results) => {
+        connectionRelease(connection); // Release the connection
+
+        if (queryErr) {
+          console.error("Database query error: 47", queryErr);
+          return callback(queryErr, null);
+        }
+
+        callback(null, results);
+      });
+    });
+  },
 };
 
 module.exports = Permission;
